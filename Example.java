7e7d7bfd57cf8dd040efae05ce2771f8e2ae38fa
Balakrishnamalga <malgabalakrishna23@gmail.com>
2023-07-28 23:45:23 -0700

    commiting example

--- Example.java
+++ Example.java
@@ -21,11 +21,22 @@ public class Example {
 		System.out.println("Division of two numers is " +k);
 		
 	}
+	 public void sub() {
+		 k=i-j;
+		 System.out.println("Subration of two numbers is  " +k);
+	 }
+	 
+	 public void mul() {
+		 k=i*j;
+		 System.out.println("Multipication of two numbers is  " +k);
+	 }
 	
 	public static void main(String[] args) {
 		Example ex = new Example();
 		ex.sum();
 		ex.div();
+		ex.sub();
+		ex.mul();
 	}
 
 }
